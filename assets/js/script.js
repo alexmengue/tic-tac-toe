ccbd831b3a30e4aee1ae507ab08182e6fa7e59d4
Alex <alexmengue1990@hotmail.com>
2023-11-06 09:32:39 -0300

    att

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let btnRef = document.querySelectorAll('.button-option');
+let popupRef = document.querySelector('.popup');
+let msgRef = document.getElementById('message');
+let newGameBtn = document.getElementById('new-game');
+let restartBtn = document.getElementById('restart');
